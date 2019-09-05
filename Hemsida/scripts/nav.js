b7e3b99699b6b29103548adafd13fa1d759df8c2
Lukas Holmberg <lukazlovicc@hotmail.com>
2019-09-05 20:21:57 +0200

    update

--- Hemsida/scripts/nav.js
+++ Hemsida/scripts/nav.js
@@ -1,16 +1,16 @@
-(function() {
-
-	var hamburger = {
+(function() 
+{
+	var hamburger = 
+	{
 		navToggle: document.querySelector('.nav-toggle'),
 		nav: document.querySelector('nav'),
 
-		doToggle: function(e) {
+		doToggle: function(e) 
+		{
 			e.preventDefault();
 			this.navToggle.classList.toggle('expanded');
 			this.nav.classList.toggle('expanded');
 		}
 	};
-
 	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-
 }());
